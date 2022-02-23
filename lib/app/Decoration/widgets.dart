cbb8164ff123aa0dc1e67d5eba9701bcd43df547
er.suheb <suheb.cse@gmail.com>
2022-02-23 10:10:27 +0530

    icons added

--- lib/app/Decoration/widgets.dart
+++ lib/app/Decoration/widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'colors/originBg.dart';
 
@@ -24,3 +25,126 @@ class ScanIcon extends StatelessWidget {
     );
   }
 }
+
+//* ---Profile Stack Widget on Home Screen-- //
+class ProfileStack extends StatelessWidget {
+  const ProfileStack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        ClipOval(
+          child: Container(
+            height: 164,
+            width: 164,
+            color: Color(0x1Affffff),
+          ),
+        ),
+        ClipOval(
+          child: Container(
+            height: 146,
+            width: 146,
+            color: Color(0x33ffffff),
+          ),
+        ),
+        ClipOval(
+          child: Container(
+            height: 124,
+            width: 124,
+            color: Color(0x40ffffff),
+          ),
+        ),
+        ClipOval(
+          child: Container(
+            child: Image.asset(
+              'assets/images/demopic.png',
+            ),
+            height: 104,
+            width: 104,
+            decoration: const BoxDecoration(
+              color: Color(0x4Dffffff),
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x40000000),
+                  offset: Offset(0, 4),
+                  blurRadius: 10,
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+//* ---Icon Cirles Stack Widget on Home Screen-- //
+
+class IconStack extends StatelessWidget {
+  const IconStack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            OverflowBox(
+              maxWidth: 536,
+              maxHeight: 536,
+              child: ClipOval(
+                child: Container(
+                  color: Color(0x0Dffffff),
+                ),
+              ),
+            ),
+            OverflowBox(
+              maxHeight: 312,
+              maxWidth: 312,
+              child: ClipOval(
+                child: Container(
+                  color: Color(0x1Affffff),
+                ),
+              ),
+            ),
+            OverflowBox(
+              maxHeight: 244,
+              maxWidth: 244,
+              child: ClipOval(
+                child: Container(
+                  color: Color(0x1Affffff),
+                ),
+              ),
+            ),
+            OverflowBox(
+              maxHeight: 155,
+              maxWidth: 155,
+              child: ClipOval(
+                child: Container(
+                  color: Color(0x1Affffff),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: const OverflowBox(
+                maxWidth: 78,
+                maxHeight: 78,
+                child: ScanIcon(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,10 @@
 import 'package:digitag/app/Decoration/colors/originBg.dart';
 import 'package:digitag/app/Decoration/decoration.dart';
+import 'package:digitag/app/Decoration/text/text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:get/get.dart';
 
@@ -22,108 +24,62 @@ class HomeView extends GetView<HomeController> {
             decoration: Decorations.grdntBG,
             child: Column(
               children: [
-                Text("Hi, Mohammad"),
-                Stack(
-                  alignment: Alignment.center,
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ClipOval(
-                      child: Container(
-                        height: 164,
-                        width: 164,
-                        color: Color(0x1Affffff),
+                    Spacer(),
+                    Container(
+                      child: customText("Hi, Mohammad", 20),
+                      decoration: const BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x40000000),
+                            offset: Offset(0, 4),
+                            blurRadius: 35,
+                          )
+                        ],
                       ),
                     ),
-                    ClipOval(
-                      child: Container(
-                        height: 146,
-                        width: 146,
-                        color: Color(0x33ffffff),
-                      ),
+                    Spacer(
+                      flex: 4,
                     ),
-                    ClipOval(
-                      child: Container(
-                        height: 124,
-                        width: 124,
-                        color: Color(0x40ffffff),
-                      ),
-                    ),
-                    ClipOval(
-                      child: Container(
-                        child: Image.asset(
-                          'assets/images/demopic.png',
-                        ),
-                        height: 104,
-                        width: 104,
-                        decoration: const BoxDecoration(
-                            color: Color(0x4Dffffff),
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x40000000),
-                                offset: Offset(0, 4),
-                                blurRadius: 10,
-                              )
-                            ]),
-                      ),
-                    ),
+                    InkWell(
+                        onTap: (() {}),
+                        child: SvgPicture.asset('assets/icons/Vector.svg')),
+                    Spacer(),
+                    InkWell(
+                        onTap: (() {}),
+                        child: SvgPicture.asset('assets/icons/Bell.svg')),
+                    Spacer(),
+                    InkWell(
+                        onTap: (() {}),
+                        child: SvgPicture.asset('assets/icons/drawer.svg')),
+                    Spacer()
                   ],
                 ),
-                SizedBox(
-                  height: 490,
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        OverflowBox(
-                          maxWidth: 536,
-                          maxHeight: 536,
-                          child: ClipOval(
-                            child: Container(
-                              color: Color(0x0Dffffff),
-                            ),
-                          ),
-                        ),
-                        OverflowBox(
-                          maxHeight: 312,
-                          maxWidth: 312,
-                          child: ClipOval(
-                            child: Container(
-                              color: Color(0x1Affffff),
-                            ),
-                          ),
-                        ),
-                        OverflowBox(
-                          maxHeight: 244,
-                          maxWidth: 244,
-                          child: ClipOval(
-                            child: Container(
-                              color: Color(0x1Affffff),
-                            ),
-                          ),
-                        ),
-                        OverflowBox(
-                          maxHeight: 155,
-                          maxWidth: 155,
-                          child: ClipOval(
-                            child: Container(
-                              color: Color(0x1Affffff),
-                            ),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {},
-                          child: OverflowBox(
-                            maxWidth: 78,
-                            maxHeight: 78,
-                            child: ScanIcon(),
-                          ),
-                        )
-                      ],
+                Spacer(),
+                Container(
+                  height: 40,
+                  width: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Color(0x26ffffff)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/MedicalSupport.svg',
                     ),
                   ),
+                ),
+                Spacer(),
+                const ProfileStack(),
+                Spacer(
+                  flex: 11,
+                ),
+                const IconStack(),
+                SizedBox(
+                  height: 65,
                 )
               ],
             ),
