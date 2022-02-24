5c6594fd7e9fd81fa0f6f1ebb74a44125f254818
Er.Suheb <suheb.cse@gmail.com>
2022-02-24 21:56:19 +0530

    Minor Changes 0.3

--- lib/app/Decoration/widgets.dart
+++ lib/app/Decoration/widgets.dart
@@ -1,6 +1,9 @@
+import 'package:digitag/app/Decoration/text/text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+import 'package:get/get.dart';
 
 import 'colors/originBg.dart';
 
@@ -172,3 +175,87 @@ class MedicalSupportIcon extends StatelessWidget {
     );
   }
 }
+//* Flutter Switch Toggle *//
+
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({
+    Key? key,
+    required this.status,
+  }) : super(key: key);
+
+  final RxBool status;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterSwitch(
+          value: status.value,
+          padding: 1,
+          onToggle: (val) {
+            if (status.value == false) {
+              status.value = true;
+              print("Audit On");
+            } else {
+              status.value = false;
+              print("Audit Off");
+            }
+          },
+          valueFontSize: 9,
+          showOnOff: true,
+         inactiveText: "Audit Off",
+         activeText: "Audit On",
+         activeTextColor: Color(0xffBAFFEE),
+         inactiveTextColor: Color(0xffBAFFEE),
+          width: 75,
+          inactiveIcon: Icon(Icons.festival,color: Colors.white,),
+        activeToggleBorder:  Border.all(width: 1, color: Color(0x663347B4)),
+          
+          height: 27,
+          inactiveColor: Color(0x80004E79),
+          activeColor: Color(0x80004E79),
+          inactiveToggleColor: Color(0x9942FFDD),
+          inactiveToggleBorder:
+              Border.all(width: 1, color: Color(0x663347B4)),
+        );
+  }
+}
+
+//* User's Name and Icons in Top Row *//
+
+class Custom_UserName_Icons extends StatelessWidget {
+  const Custom_UserName_Icons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Spacer(),
+        Container(
+          child: customText("Hi, Mohammad", 20),
+          
+        ),
+        const Spacer(
+          flex: 4,
+        ),
+        InkWell(
+            onTap: (() {
+              
+
+            }),
+            child: SvgPicture.asset('assets/icons/Vector.svg')),
+        const Spacer(),
+        InkWell(
+            onTap: (() {}),
+            child: SvgPicture.asset('assets/icons/Bell.svg')),
+        const Spacer(),
+        InkWell(
+            onTap: (() {}),
+            child: SvgPicture.asset('assets/icons/drawer.svg')),
+        const Spacer()
+      ],
+    );
+  }
+}
+
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -30,71 +30,18 @@ class HomeView extends GetView<HomeController> {
                 SizedBox(
                   height: 56,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Spacer(),
-                    Container(
-                      child: customText("Hi, Mohammad", 20),
-                      decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x40000000),
-                            offset: Offset(0, 4),
-                            blurRadius: 35,
-                          )
-                        ],
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 4,
-                    ),
-                    InkWell(
-                        onTap: (() {}),
-                        child: SvgPicture.asset('assets/icons/Vector.svg')),
-                    const Spacer(),
-                    InkWell(
-                        onTap: (() {}),
-                        child: SvgPicture.asset('assets/icons/Bell.svg')),
-                    const Spacer(),
-                    InkWell(
-                        onTap: (() {}),
-                        child: SvgPicture.asset('assets/icons/drawer.svg')),
-                    const Spacer()
-                  ],
-                ),
-                const Spacer(),
+                Custom_UserName_Icons(),
+                                SizedBox(height: 30,)
+,
                 const MedicalSupportIcon(),
-                const Spacer(),
+                SizedBox(height: 30,),
                 const ProfileStack(),
                 const SizedBox(
-                  height: 23,
+                  height: 40,
                 ),
-                Obx(() => FlutterSwitch(
-                      value: status.value,
-                      padding: 2,
-                      onToggle: (val) {
-                        if (status.value == false) {
-                          status.value = true;
-                          print("Audit On");
-                        } else {
-                          status.value = false;
-                          print("Audit Off");
-                        }
-                      },
-                      showOnOff: true,
-                      inactiveText: "Audit",
-                      activeText: "Audit",
-                      width: 73,
-                      height: 27,
-                      inactiveColor: Color(0x80004E79),
-                      activeColor: Color(0x80004E79),
-                      inactiveToggleColor: Color(0x9942FFDD),
-                      inactiveToggleBorder:
-                          Border.all(width: 1, color: Color(0x663347B4)),
-                    )),
+                Obx(() => CustomSwitch(status: status)),
                 const SizedBox(
-                  height: 300,
+                  height: 360,
                 ),
                 const IconStack(),
                 const SizedBox(
@@ -108,3 +55,4 @@ class HomeView extends GetView<HomeController> {
     );
   }
 }
+
