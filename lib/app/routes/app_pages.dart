0aa65b07b59d6caf6c714dd45a746e3af14937d2
Sparx <60690248+SparxitSolution09@users.noreply.github.com>
2022-04-13 12:26:52 +0530

    Testing git

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,15 +1,5 @@
 import 'package:get/get.dart';
 
-<<<<<<< HEAD
-import '../modules/bindings/drawer_binding.dart';
-import '../modules/views/drawer_view.dart';
-import '../modules/bindings/home_binding.dart';
-import '../modules/views/home_view.dart';
-import '../modules/bindings/login_binding.dart';
-import '../modules/views/login_view.dart';
-import '../modules/bindings/profile_binding.dart';
-import '../modules/views/profile_view.dart';
-=======
 import '../modules/drawer/bindings/drawer_binding.dart';
 import '../modules/drawer/views/drawer_view.dart';
 import '../modules/home/bindings/home_binding.dart';
@@ -18,18 +8,13 @@ import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
 import '../modules/profile/bindings/profile_binding.dart';
 import '../modules/profile/views/profile_view.dart';
->>>>>>> edf43e46e477e26275465a8cb62e0cd2a5432f3e
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-<<<<<<< HEAD
   static const INITIAL = Routes.DRAWER;
-=======
-  static const INITIAL = Routes.PROFILE;
->>>>>>> edf43e46e477e26275465a8cb62e0cd2a5432f3e
 
   static final routes = [
     GetPage(
