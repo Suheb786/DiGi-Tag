d66a08a8a5c83f4a57e7fb4782a14d43fc268926
saffer4u <aftab.alam4u71@gmail.com>
2022-04-26 18:44:57 +0530

    Medical Support Image picker fixed

--- lib/app/modules/controllers/medialsupport_controller.dart
+++ lib/app/modules/controllers/medialsupport_controller.dart
@@ -1,18 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-class MedialsupportController extends GetxController {
+class MedicalsupportController extends GetxController {
   TextEditingController title = TextEditingController();
 
   //TODO: Implement MedialsupportController
-  XFile? imagepath;
+  File? pickedImage;
   final ImagePicker picker = ImagePicker();
 
   String? titleValidation(String value) {
     if (value.isEmpty) {
       return "Enter your User ID";
     }
+  }
+
+  void picImageCamera(BuildContext context) async {
+    final pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (pickedFile != null) {
+      pickedImage = File(pickedFile.path);
+    } else {
+      pickedImage = null;
+    }
+    update();
   }
 
   final count = 0.obs;
